ad739e483832e6f4ba5f8fa89b42288b78dc9e1b
PaoAndreCM <paoandrecm@gmail.com>
2021-11-10 22:12:36 +0100

    10 elements added to ArrayList of Nodes in Main. Testprogram prints unsorted element values

--- src/worksheet1/Main.java
+++ src/worksheet1/Main.java
@@ -6,11 +6,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		start();
-		ArrayList<Node> DataSet = new ArrayList<Node>();
+	
 		MensAccesories sunglasses = new MensAccesories(9);
-		DataSet.add(sunglasses);
+		MensAccesories ties = new MensAccesories(26);
+		MensAccesories cufflinks = new MensAccesories(16);
+		MensAccesories wallets = new MensAccesories(34);
+		MensAccesories belts = new MensAccesories(31);
+		MensAccesories hats = new MensAccesories(5);
+		MensAccesories gloves = new MensAccesories(10);
+		MensAccesories scarves = new MensAccesories(18);
+		MensAccesories handkerchiefs = new MensAccesories(46);
+		MensAccesories watches = new MensAccesories(12);
 		
-		System.out.println(DataSet.get(0).getKey());
+		ArrayList<Node> DataSet = new ArrayList<Node>();   //Should this be an arrayList of Nodes or of MensAccessories?
+		
+		DataSet.add(sunglasses);
+		DataSet.add(ties);
+		DataSet.add(cufflinks);
+		DataSet.add(wallets);
+		DataSet.add(belts);
+		DataSet.add(hats);
+		DataSet.add(gloves);
+		DataSet.add(scarves);
+		DataSet.add(handkerchiefs);
+		DataSet.add(watches);
+		
+		for(Node Datum : DataSet)      // connected to previous comment: Node? or MensAccessories?
+		{
+		    System.out.println(Datum.getKey());
+		}
 
 	}
 
