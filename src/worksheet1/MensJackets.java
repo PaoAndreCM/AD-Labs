a9124c254803e909bfab7af1739ca041dc0e257e
shamilkaW <shamilka2000@gmail.com>
2021-11-10 18:09:22 +0100

    Added 4 more classes

---
+++ src/worksheet1/MensJackets.java
@@ -0,0 +1,11 @@
+package worksheet1;
+
+public class MensJackets implements Node{
+
+	@Override
+	public int getKey() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/worksheet1/MensPJs.java
@@ -0,0 +1,11 @@
+package worksheet1;
+
+public class MensPJs implements Node{
+
+	@Override
+	public int getKey() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
